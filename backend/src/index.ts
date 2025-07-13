6edbd26ade26f7e54d26d3b80c8a46764d0bc16a
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-07-14 02:33:10 +0300

    fixed missing port in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,14 @@ import { PrismaClient } from "@prisma/client";
 import blogRouter from "./routes/blog.route";
 import authenticateToken from "./middlewares/authMiddleware";
 import userRouter from "./routes/user.route";
-import validateblogdetails from "./middlewares/validateblogdetails";
-import { updateBlog } from "./controllers/blogs.controller";
 
 import authRouter from "./routes/auth.route";
 
+const port: number = parseInt(process.env.PORT as string, 10) || 5000;
+
+
 const app = express();
 app.use(express.json());
-const client = new PrismaClient();
 
 app.use(
   cors({
@@ -29,41 +29,7 @@ app.get("/", (req, res) => {
   res.send(`<h1>Welcome to Bloggit</h1>`);
 });
 
-app.post("/api/auth/logout", (req, res) => {
-  res.send("logout successful");
-});
 
-app.get("/api/blogs", (req, res) => {
-  res.send("returned all posts");
+app.listen(port, () => {
+  console.log(` Server is running on http://localhost:${port}`);
 });
-
-app.post("/api/blogs", (req, res) => {
-  res.send("get all posts");
-});
-
-app.get("/api/blogs/:blogId", (req, res) => {
-  "get a specific post";
-});
-
-app.patch("/api/blogs/:blogId", (req, res) => {
-  "update a specific post";
-});
-
-app.delete("/api/blogs/:blogId", (req, res) => {
-  "deleted a specific post";
-});
-
-app.patch("/api/user", (req, res) => {
-  "update a users info";
-});
-
-app.patch("/api/user/password", (req, res) => {
-  "update a users password";
-});
-
-app.get("/api/user/blogs", (req, res) => {
-  "get all blogs belonging to a specific user";
-});
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => 
