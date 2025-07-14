49cad85f10d16386a49082838891e0589737a4ae
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-07-14 23:01:57 +0300

    fix deployment package issues

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@
 //   plugins: [react()],
 // });
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
